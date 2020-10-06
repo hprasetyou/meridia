75cbdb21bdd7489a0877221935fe02f2340a01ce
Patrick Star <hprasetyou@gmail.com>
2020-10-06 17:04:00 +0700

    lightbox nav

--- components/element/photoGallery.vue
+++ components/element/photoGallery.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
-    <div class="flex flex-wrap">
-      <div v-for="(image, i) in images" :key="i" :class="'flex' + generateClass(image.attr)" alt="">
-        <img :src="require('~/static/img/' + image.img)" class="mt-auto" alt="">
+    <div v-if="images.length > 0" v-show="showLbImage" class="lightbox-image fixed flex inset-0 z-50">
+      <img :src="require('~/static/img/' + images[lbImage].img)" :class="`m-auto ${showLbImage ? 'opacity-100' : 'opacity-0'} transition-opacity duration-500 ease-in-out`" alt="">
+      <div class="lightbox-image--nav">
+        <span class="lightbox-image--next" @click="lbImage = (lbImage + 1 == images.length) ? 0 : lbImage + 1" />
+        <span class="lightbox-image--prev" @click="lbImage = ((lbImage - 1) < 0) ? images.length - 1 : lbImage - 1" />
+      </div>
+      <div class="lightbox-image--close" @click="showLbImage=false">
+        <span />
+        <span />
       </div>
     </div>
-    <div v-if="showImage" class="big-image">
-      <img src="" alt="">
+    <div class="flex flex-wrap">
+      <div v-for="(image, i) in images" :key="i" :class="'flex' + generateClass(image.attr)" alt="" @click="setLightImage(i)">
+        <img :src="require('~/static/img/' + image.img)" class="mt-auto" alt="">
+      </div>
     </div>
   </div>
 </template>
@@ -22,7 +30,8 @@ export default {
   },
   data () {
     return {
-      showImage: false
+      lbImage: 0,
+      showLbImage: false
     }
   },
   methods: {
@@ -54,7 +63,86 @@ export default {
         }
       }
       return wClass
+    },
+    setLightImage (i) {
+      this.lbImage = i
+      this.showLbImage = true
     }
   }
 }
 </script>
+<style lang="scss">
+  .lightbox-image{
+    img{
+      max-width: 90%;
+      max-height: 95%;
+    }
+    &::before{
+      content: '';
+      position: absolute;
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
+      display: block;
+      background: #00000094;
+      z-index: -1;
+    }
+  }
+  .lightbox-image--close{
+    width: 30px;
+    height: 30px;
+    position: absolute;
+    top: 30px;
+    right: 30px;
+    span{
+      display: block;
+      width: 40px;
+      height: 2px;
+      background: #fff;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      cursor: pointer;
+      transform: translate(-50%, -50%) rotate(45deg);
+      &:last-child{
+        transform: translate(-50%, -50%) rotate(-45deg);
+      }
+    }
+  }
+  .lightbox-image{
+    &--next,
+    &--prev{
+      display: block;
+      width: 100px;
+      top: 0;
+      bottom: 0;
+      position: absolute;
+      &::before{
+        display: block;
+        content: '';
+        width: 30px;
+        height: 30px;
+        border: 2px solid #fff;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%) rotate(45deg);
+      }
+    }
+    &--prev{
+      left: 0;
+      &::before{
+        border-top-color: transparent;
+        border-right-color: transparent;
+      }
+    }
+    &--next{
+      right: 0;
+      &::before{
+        border-bottom-color: transparent;
+        border-left-color: transparent;
+      }
+    }
+  }
+</style>
