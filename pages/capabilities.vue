0b6b5fc15fb797e7680fa6eab8567626b54a1a38
Patrick Star <hprasetyou@gmail.com>
2020-10-07 15:28:28 +0700

    capabilities page

---
+++ pages/capabilities.vue
@@ -0,0 +1,21 @@
+<template>
+  <our-service :services="services" />
+</template>
+<script>
+import ourService from '../components/pageSection/OurService'
+import data from '../components/data/data'
+
+export default {
+  components: {
+    ourService
+  },
+  computed: {
+    services () {
+      return data.projects.map((item) => {
+        item.link = 'contacts'
+        return item
+      })
+    }
+  }
+}
+</script>
